361275d19c6c1b561418b8bdfd8164bad247b281
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-07-08 15:42:03 +0900

    cssリンク修正4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatGptController;
+use App\Http\Controllers\ChatbotController;
 
 Route::get('/', function () {
     return view('welcome');

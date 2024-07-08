b437a3622ae19f82aa8d2b992df32b13780e7815
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-07-08 16:14:13 +0900

    cssリンク修正8

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,7 +5,6 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link href="{{ asset('css/app.css') }}">
     <link href="{{ asset('css/style.css') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{-- @vite('resources/css/app.css') --}}
 </head>
 
